8704fae9803ec2174ae5be14a9d3ca98f3eacd71
Masahiro Arakane <arakane.masahiro@gmail.com>
2018-02-27 13:51:26 +0900

    csv and json data with D3

--- app.js
+++ app.js
@@ -11,21 +11,51 @@
 //     .text('Hello World')
 //     .style('color', 'blue');
 
-var dataset = [10, 20, 30, 40, 50];
-var el = d3.select('body')
-    .selectAll('p')
-    .data(dataset)
-    .enter()
-    .append('p')
-    .text(function (d) {
-        return 'This paragraph is binded to the number ' + d;
-    })
-    .style('color', function (d) {
-        if (d > 25) {
-            return 'red';
-        } else {
-            return 'blue';
-        }
-    });
+d3.csv('data.csv', function (err, data) {
+   if (err) {
+       return console.log(err);
+   }
+   console.log(data);
+   // generate(data.columns);
+});
 
-console.log(el);
+d3.json('data.json', function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    // generate(data);
+});
+
+
+function generate(dataset) {
+    // var dataset = [10, 20, 30, 40, 50];
+
+    var el = d3.select('body')
+        .selectAll('p')
+        .data(dataset)
+        .enter()
+        .append('p')
+        .text(function (d) {
+            return 'This paragraph is binded to the number ' + d;
+        })
+        .attr('class', function (d) {
+            if (d > 25) {
+                return 'foo';
+            } else {
+                return null;
+            }
+        })
+        .classed('bar', function (d) {
+            return d < 25;
+        })
+        .style('color', function (d) {
+            if (d > 25) {
+                return 'red';
+            } else {
+                return 'blue';
+            }
+        });
+
+    console.log(el);
+}
+
